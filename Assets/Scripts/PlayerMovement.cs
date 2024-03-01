d3225cd70835f563d2470ee8120fde425d7314c3
(20105918) Matthew Hurley <20105918@IT103iMAC09.wit.ie>
2024-03-01 13:57:33 +0000

    week 6 lab tweeks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,16 @@ public class PlayerMovement : MonoBehaviour
         moveInput = input.Get<Vector2>();
         Debug.Log(moveInput);
     }
+    void OnJump(InputValue input)
+    {
+        RaycastHit2D ray = Physics2D.Raycast(myRigidBody.transform.position + (Vector3.down / 2), Vector2.down, 0.1f);
+        print(ray.distance);
+        if (ray.distance == 0)
+        {
+            myRigidBody.velocity += Vector2.up * 5;
+            
+        }
+    }
     private void Run()
     {
         playerVeloicity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
