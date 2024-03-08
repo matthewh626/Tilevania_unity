b5449f8603db9dda2b0fcefc50698058e931954e
(20105918) Matthew Hurley <20105918@IT103iMAC09.wit.ie>
2024-03-08 12:48:51 +0000

    week 7 labwork; fixed jumping added climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,36 +8,58 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Vector2 playerVeloicity;
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    CapsuleCollider2D myCollider;
     // Start is called before the first frame update
+
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-
+        myCollider = GetComponent<CapsuleCollider2D>();
     }
-   void OnMove(InputValue input)
+
+    void OnMove(InputValue input)
     {
         moveInput = input.Get<Vector2>();
         Debug.Log(moveInput);
     }
+
     void OnJump(InputValue input)
     {
-        RaycastHit2D ray = Physics2D.Raycast(myRigidBody.transform.position + (Vector3.down / 2), Vector2.down, 0.1f);
-        print(ray.distance);
-        if (ray.distance == 0)
+       
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) )
         {
-            myRigidBody.velocity += Vector2.up * 5;
+            myRigidBody.velocity += Vector2.up * jumpSpeed;
             
         }
     }
+
     private void Run()
     {
         playerVeloicity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVeloicity;
         myAnimator.SetBool("Running", Mathf.Abs(myRigidBody.velocity.x)>0);
     }
+
+    void ClimbLadder()
+    {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidBody.gravityScale = 9.8f;
+            return;
+
+        }
+        myRigidBody.gravityScale = 0;
+        playerVeloicity = new Vector2(myRigidBody.velocity.x, moveInput.y * climbSpeed);
+        myRigidBody.velocity = playerVeloicity;
+        myAnimator.SetBool("Climbing", Mathf.Abs(myRigidBody.velocity.y) > 0);
+    }
+
     void FlipSprite()
     {
         if (Mathf.Abs(myRigidBody.velocity.x) > 0)
@@ -47,9 +69,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     // Update is called once per frame
+
     void Update()
     {
         Run();
+        ClimbLadder();
         FlipSprite();
     }
 }
